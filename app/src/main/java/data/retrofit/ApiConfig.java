05af1b382d335ac00d452578ed46aa4a062beb96
muhammadhidayatullah22 <12250112899@students.uin-suska.ac.id>
2024-04-21 13:50:17 +0700

    Initial commit

---
+++ app/src/main/java/data/retrofit/ApiConfig.java
@@ -0,0 +1,27 @@
+package data.retrofit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiConfig {
+
+    public static ApiService getApiService() {
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY); // Tingkatkan level logging jika diperlukan
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://restaurant-api.dicoding.dev/")
+                .addConverterFactory(GsonConverterFactory.create()) // Menggunakan GsonConverterFactory untuk mengkonversi JSON ke objek Java
+                .client(client)
+                .build();
+
+        return retrofit.create(ApiService.class);
+    }
+
+}
